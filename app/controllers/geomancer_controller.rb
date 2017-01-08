c246763f211746f1dafa428b735e60a02fc4a85b
Andrew Shields <shieldsa13@gmail.com>
2017-01-07 21:26:25 -0500

    Add geomancer controller

---
+++ app/controllers/geomancer_controller.rb
@@ -0,0 +1,2 @@
+class GeomancerController < ApplicationController
+end
---
+++ test/controllers/geomancer_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GeomancerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
