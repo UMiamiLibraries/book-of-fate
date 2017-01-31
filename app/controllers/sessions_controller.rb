8cedde0a3dbdd6a9d8255f65e733b3a4d8a3c018
Andrew Shields <shieldsa13@gmail.com>
2017-01-31 17:40:47 -0500

    change login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to questions_path
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      redirect_to questions_path
+      render 'new'
     end
   end
 
